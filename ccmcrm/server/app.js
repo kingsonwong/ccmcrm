7469e08f9130fcdc94ffcd63f2db40b5825daf00
kingson29 <50691571+kingson29@users.noreply.github.com>
2020-03-09 18:49:32 +0800

    api setup

--- ccmcrm/server/app.js
+++ ccmcrm/server/app.js
@@ -1,8 +1,32 @@
 var express = require("express");
 var app = express();
+const sqlite3 = require("sqlite3").verbose();
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
+let db = new sqlite3.Database("../ccmdb.db", err => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the cocomall database.");
+});
+
+app.get("/api/getorders", function(req, res) {
+  res.send("SELECT * FROM orders");
+});
+
+app.get("/api/getorder:id", function(req, res) {
+  res.send("SELECT * FROM orders WHERE order_id = param_order_id");
+});
+
+app.post("/api/postorder:id", function(req, res) {
+  res.send("SELECT * FROM orders");
+});
+
+app.post("/api/putorder:id", function(req, res) {
+  res.send("SELECT * FROM orders");
+});
+
+app.post("/api/deleteorder:id", function(req, res) {
+  res.send("SELECT * FROM orders");
 });
 
 app.listen(3001, function() {
